d9c4d461c139712d7f9b3cca06da33d344b2516e
Brayden Zee <github@blueish.dev>
2024-12-06 20:02:36 -0500

    Day 6 Part 2

--- day6/main.hs
+++ day6/main.hs
@@ -1,3 +1,5 @@
+import Data.List (sortBy)
+
 skipWhile :: (a -> Bool) -> [a] -> [a]
 skipWhile _ [] = []
 skipWhile shouldSkip (start : rest) =
@@ -27,6 +29,15 @@ skip _ [] = []
 skip 0 rest = rest
 skip amount_to_skip (_ : rest) = skip (amount_to_skip - 1) rest
 
+data Direction = Up | Down | Left | Right
+  deriving (Eq, Show)
+
+compareRows :: (Int, Int) -> (Int, Int) -> Ordering
+compareRows (fst, _) (snd, _) = fst `compare` snd
+
+compareCols :: (Int, Int) -> (Int, Int) -> Ordering
+compareCols (_, fst) (_, snd) = fst `compare` snd
+
 setVisited :: [[Char]] -> Int -> Int -> [[Char]]
 setVisited input row col =
   let aboveRows = take row input
@@ -36,38 +47,67 @@ setVisited input row col =
       afterCols = skip (col + 1) currentRow
    in aboveRows ++ [beforeCols ++ ['V'] ++ afterCols] ++ belowRows
 
-moveUp :: [[Char]] -> Int -> Int -> Int
+moveUp :: [[Char]] -> Int -> Int -> [(Int, Int)]
 moveUp input row col
-  | row == 0 = if input !! row !! col == 'V' then 0 else 1
+  | row == 0 = if input !! row !! col == 'V' then [] else [(row, col)]
   | input !! (row - 1) !! col == '#' = moveRight input row col
-  | otherwise = (if input !! row !! col == 'V' then 0 else 1) + moveUp (setVisited input row col) (row - 1) col
+  | otherwise = (if input !! row !! col == 'V' then [] else [(row, col)]) ++ moveUp (setVisited input row col) (row - 1) col
 
-moveDown :: [[Char]] -> Int -> Int -> Int
+moveDown :: [[Char]] -> Int -> Int -> [(Int, Int)]
 moveDown input row col
-  | row == length input - 1 = if input !! row !! col == 'V' then 0 else 1
+  | row == length input - 1 = if input !! row !! col == 'V' then [] else [(row, col)]
   | input !! (row + 1) !! col == '#' = moveLeft input row col
-  | otherwise = (if input !! row !! col == 'V' then 0 else 1) + moveDown (setVisited input row col) (row + 1) col
+  | otherwise = (if input !! row !! col == 'V' then [] else [(row, col)]) ++ moveDown (setVisited input row col) (row + 1) col
 
-moveLeft :: [[Char]] -> Int -> Int -> Int
+moveLeft :: [[Char]] -> Int -> Int -> [(Int, Int)]
 moveLeft input row col
-  | col == 0 = if input !! row !! col == 'V' then 0 else 1
+  | col == 0 = if input !! row !! col == 'V' then [] else [(row, col)]
   | input !! row !! (col - 1) == '#' = moveUp input row col
-  | otherwise = (if input !! row !! col == 'V' then 0 else 1) + moveLeft (setVisited input row col) row (col - 1)
+  | otherwise = (if input !! row !! col == 'V' then [] else [(row, col)]) ++ moveLeft (setVisited input row col) row (col - 1)
 
-moveRight :: [[Char]] -> Int -> Int -> Int
+moveRight :: [[Char]] -> Int -> Int -> [(Int, Int)]
 moveRight input row col
-  | col == length (head input) - 1 = if input !! row !! col == 'V' then 0 else 1
+  | col == length (head input) - 1 = if input !! row !! col == 'V' then [] else [(row, col)]
   | input !! row !! (col + 1) == '#' = moveDown input row col
-  | otherwise = (if input !! row !! col == 'V' then 0 else 1) + moveRight (setVisited input row col) row (col + 1)
+  | otherwise = (if input !! row !! col == 'V' then [] else [(row, col)]) ++ moveRight (setVisited input row col) row (col + 1)
+
+doesLoop :: [(Int, Int)] -> Int -> Int -> Direction -> [(Int, Int)] -> Bool
+doesLoop obstacles row col currentDirection previousPlaces
+  | (row, col) `elem` previousPlaces = True
+  | otherwise =
+      let nextObstacles = case currentDirection of
+            Main.Up -> sortBy (flip compareRows) $ filter ((< row) . fst) $ filter ((== col) . snd) obstacles
+            Main.Down -> sortBy compareRows $ filter ((> row) . fst) $ filter ((== col) . snd) obstacles
+            Main.Left -> sortBy (flip compareCols) $ filter ((< col) . snd) $ filter ((== row) . fst) obstacles
+            Main.Right -> sortBy compareCols $ filter ((> col) . snd) $ filter ((== row) . fst) obstacles
+       in ( not (null nextObstacles)
+              && ( let (nextObstacleRow, nextObstacleCol) = head nextObstacles
+                       (nextRow, nextCol) = case currentDirection of
+                         Main.Up -> (nextObstacleRow + 1, nextObstacleCol)
+                         Main.Down -> (nextObstacleRow - 1, nextObstacleCol)
+                         Main.Left -> (nextObstacleRow, nextObstacleCol + 1)
+                         Main.Right -> (nextObstacleRow, nextObstacleCol - 1)
+                       nextDirection = case currentDirection of
+                         Main.Up -> Main.Right
+                         Main.Down -> Main.Left
+                         Main.Left -> Main.Up
+                         Main.Right -> Main.Down
+                    in doesLoop obstacles nextRow nextCol nextDirection (if row == nextRow && col == nextCol then previousPlaces else (row, col) : previousPlaces)
+                 )
+          )
 
 main = do
   contents <- getContents
   let input = split (== '\n') contents
+      inputWithIdx =
+        flatten $
+          map (\(row_idx, row) -> map (\(col_idx, value) -> (row_idx, col_idx, value)) row) $
+            enumerate $
+              map enumerate input
+      obstacles = map (\(a, b, _) -> (a, b)) $ filter (\(_, _, val) -> val == '#') inputWithIdx
       (startingRow, startingCol, _) =
         head $
-          filter (\(_, _, val) -> val == '^') $
-            flatten $
-              map (\(row_idx, row) -> map (\(col_idx, value) -> (row_idx, col_idx, value)) row) $
-                enumerate $
-                  map enumerate input
-   in print $ moveUp input startingRow startingCol
+          filter (\(_, _, val) -> val == '^') inputWithIdx
+      possibleObstacles = moveUp input startingRow startingCol
+   in do
+        print $ length $ filter (\possibleObstacle -> doesLoop (possibleObstacle : obstacles) startingRow startingCol Main.Up []) possibleObstacles
