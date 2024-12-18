264b8fa5b98fee860cb27ba11d1a66e028327040
Brayden Zee <github@blueish.dev>
2024-12-18 10:29:30 -0500

    Day 18 Part 2

--- day18/main.hs
+++ day18/main.hs
@@ -18,14 +18,15 @@ split predicate list =
       remaining = skipWhile (not . predicate) processedList
    in if null processedList then [] else firstGroup : split predicate remaining
 
-bfs :: Set.Set (Int, Int) -> [(Int, Int, Int)] -> Int
-bfs barriersOrVisited ((x, y, cost) : remaining)
-  | x == width - 1 && y == height - 1 = cost
+bfs :: Set.Set (Int, Int) -> [(Int, Int)] -> Bool
+bfs _ [] = False
+bfs barriersOrVisited ((x, y) : remaining)
+  | x == width - 1 && y == height - 1 = True
   | otherwise =
       let surrounding = filter (\(x, y) -> x >= 0 && y >= 0 && x < width && y < height && Set.notMember (x, y) barriersOrVisited) [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-       in bfs (foldr Set.insert barriersOrVisited surrounding) (remaining ++ map (\(x, y) -> (x, y, cost + 1)) surrounding)
+       in bfs (foldr Set.insert barriersOrVisited surrounding) (remaining ++ surrounding)
 
 main = do
   contents <- getContents
   let barriers :: [(Int, Int)] = map ((\[x, y] -> (x, y)) . map read . split (== ',')) $ lines contents
-  print $ bfs (Set.fromList $ take 1024 barriers) [(0, 0, 0)]
+  print $ barriers !! (head (filter (not . (\amount -> bfs (Set.fromList $ take amount barriers) [(0, 0)])) [0 .. length barriers]) - 1)
