dbc10957946febc2f57e37ac2f76ab8149875c0c
Brayden Zee <github@blueish.dev>
2024-12-17 22:04:57 -0500

    Day 17 Part 2
    This probably only works on my input case

--- day17/main.hs
+++ day17/main.hs
@@ -1,4 +1,6 @@
-import Data.Bits (xor)
+import Data.Bits
+import Data.List (minimumBy)
+import Data.Maybe (fromJust, mapMaybe)
 import Debug.Trace
 
 skip :: Int -> [a] -> [a]
@@ -19,6 +21,11 @@ split shouldSplit list =
       firstGroup = takeWhile (not . shouldSplit) processedList
       remaining = skipWhile (not . shouldSplit) processedList
    in if null processedList then [] else firstGroup : split shouldSplit remaining
+
+numDigits :: Int -> Int
+numDigits val =
+  let digits = logBase 10 $ fromIntegral val
+   in floor $ digits + 1
 
 runComputer :: [Int] -> Int -> Int -> Int -> Int -> [Int]
 runComputer program pc a b c
@@ -45,12 +52,48 @@ runComputer program pc a b c
             6 -> runComputer program (pc + 2) a (a `div` (2 ^ comboOperand)) c
             7 -> runComputer program (pc + 2) a b (a `div` (2 ^ comboOperand))
 
+findA :: [Int] -> Int -> Int -> [[Int]]
+findA [] a _ = [[a]]
+findA (rawTarget : remaining) a aMap =
+  let target = rawTarget `xor` 6
+      filterAMod8 aMod8 = aMod8 .&. (aMap .&. 7) == a .&. (aMap .&. 7)
+      filterShiftedValue aMod8 =
+        let shiftedAmount = aMod8 `xor` 3
+            requiredShiftedValue = target `xor` aMod8
+            actualShiftedValue = a `shiftR` shiftedAmount
+            shiftedMap = (aMap `shiftR` shiftedAmount) .&. 7
+         in requiredShiftedValue .&. shiftedMap == actualShiftedValue .&. shiftedMap
+      results =
+        flatten
+          $ filter (not . null)
+          $ map
+            ( \aMod8 ->
+                let shiftAmount = aMod8 `xor` 3
+                    requiredShiftedValue = target `xor` aMod8
+                    newA = a .|. (requiredShiftedValue `shiftL` shiftAmount)
+                    newAMap = aMap .|. (7 `shiftL` shiftAmount)
+                 in map (aMod8 :) $ findA remaining (newA `shiftR` 3) (newAMap `shiftR` 3)
+            )
+          $ filter
+            (\aMod8 -> filterAMod8 aMod8 && filterShiftedValue aMod8)
+            [0 .. 7]
+   in results
+
+flatten :: [[a]] -> [a]
+flatten [] = []
+flatten [[]] = []
+flatten ([] : (next : innerRemaining) : remaining) = next : flatten (innerRemaining : remaining)
+flatten ((next : innerRemaining) : remaining) = next : flatten (innerRemaining : remaining)
+
+enumerate :: [a] -> [(Int, a)]
+enumerate [] = []
+enumerate (first : remaining) = (0, first) : map (\(idx, val) -> (idx + 1, val)) (enumerate remaining)
+
 main = do
   contents <- getContents
   let [rawA, rawB, rawC, rawProgram] = filter (not . null) $ lines contents
-      a :: Int = read $ skip 12 rawA
       b :: Int = read $ skip 12 rawB
       c :: Int = read $ skip 12 rawC
       program :: [Int] = map read $ split (== ',') $ skip 9 rawProgram
-      result = foldr1 (\a b -> a ++ "," ++ b) $ map show $ runComputer program 0 a b c
-  print result
+      result = filter (\a -> runComputer program 0 a b c == program) $ map (sum . map (\(idx, val) -> val `shiftL` (idx * 3)) . enumerate) $ findA program 0 0
+  print $ minimum result
