da99099c6af50a1d3a3cab441e58e99cd999f1fd
Brayden Zee <github@blueish.dev>
2024-12-19 14:49:46 -0500

    Day 19 Part 2

--- day19/main.hs
+++ day19/main.hs
@@ -26,14 +26,14 @@ skip _ [] = []
 skip 0 rest = rest
 skip amountToSkip (_ : rest) = skip (amountToSkip - 1) rest
 
-isPossibleDesign :: Map.Map Int [Int] -> Int -> Bool
-isPossibleDesign towels len =
+numWaysPossible :: Map.Map Int [Int] -> Int -> Int
+numWaysPossible towels len =
   head $
     foldr
       ( \idx acc ->
-          any (\length -> acc !! (length - 1)) (fromMaybe [] (Map.lookup idx towels)) : acc
+          sum (maybe [] (map (\length -> acc !! (length - 1))) (Map.lookup idx towels)) : acc
       )
-      [True]
+      [1]
       [0 .. len - 1]
 
 findOccurrences :: Int -> String -> String -> [Int]
@@ -50,14 +50,8 @@ findMatchingSections design towels = foldr1 (++) $ map (\towel -> let idxs = fin
 accumulateSections :: [(Int, Int)] -> Map.Map Int [Int]
 accumulateSections = foldr (\(key, val) acc -> if Map.member key acc then Map.adjust (val :) key acc else Map.insert key [val] acc) Map.empty
 
-loop :: [String] -> [String] -> IO ()
-loop [] _ = return ()
-loop (design : remaining) towels = do
-  print $ accumulateSections $ findMatchingSections design towels
-  loop remaining towels
-
 main = do
   contents <- getContents
   let [[rawTowels], designs] = split null $ lines contents
       towels = split (== ',') $ filter (/= ' ') rawTowels
-  print $ length $ filter (\design -> isPossibleDesign (accumulateSections $ findMatchingSections design towels) (length design)) designs
+  print $ sum $ map (\design -> numWaysPossible (accumulateSections $ findMatchingSections design towels) (length design)) designs
