4c76ff1826c1776350cc3f74f4de0eac83f75bb2
Brayden Zee <github@blueish.dev>
2024-12-23 18:45:28 -0500

    Day 23 Part 2

--- day23/main.hs
+++ day23/main.hs
@@ -1,6 +1,7 @@
 import Data.List
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+import Debug.Trace
 
 skipWhile :: (a -> Bool) -> [a] -> [a]
 skipWhile _ [] = []
@@ -19,21 +20,50 @@ split shouldSplit list =
       remaining = skipWhile (not . shouldSplit) processedList
    in if null processedList then [] else firstGroup : split shouldSplit remaining
 
-listToMapOfLists :: (Ord a) => [(a, b)] -> Map.Map a [b]
-listToMapOfLists = foldr (\(key, value) acc -> if key `Map.member` acc then Map.adjust (value :) key acc else Map.insert key [value] acc) Map.empty
+listToMapOfSets :: (Ord a, Ord b) => [(a, b)] -> Map.Map a (Set.Set b)
+listToMapOfSets = foldr (\(key, value) acc -> if key `Map.member` acc then Map.adjust (Set.insert value) key acc else Map.insert key (Set.fromList [value]) acc) Map.empty
 
-findPossibleLoops :: Map.Map String [String] -> String -> Int -> [[String]]
-findPossibleLoops connectionsMap target 0 = map (\connection -> [target, connection]) $ connectionsMap Map.! target
+findPossibleLoops :: Map.Map String (Set.Set String) -> String -> Int -> [[String]]
+findPossibleLoops connectionsMap target 0 = map (\connection -> [target, connection]) $ Set.toList $ connectionsMap Map.! target
 findPossibleLoops connectionsMap target depth =
-  map (target :) $ flatten $ map (\nextConnection -> findPossibleLoops connectionsMap nextConnection (depth - 1)) $ connectionsMap Map.! target
+  map (target :) $ flatten $ map (\nextConnection -> findPossibleLoops connectionsMap nextConnection (depth - 1)) $ Set.toList $ connectionsMap Map.! target
 
-findLoops :: Map.Map String [String] -> String -> Set.Set [String]
+findLoops :: Map.Map String (Set.Set String) -> String -> Set.Set [String]
 findLoops connectionsMap target = Set.fromList $ map (sort . take 3) $ filter (\val -> last val == head val) $ findPossibleLoops connectionsMap target 2
+
+findBiggestConnection :: Map.Map String (Set.Set String) -> String -> Set.Set String -> [String]
+findBiggestConnection connectionsMap current visited
+  | current `Map.notMember` connectionsMap = []
+  | otherwise =
+      let possibilities =
+            map (\connection -> findBiggestConnection connectionsMap connection (Set.insert connection visited)) $
+              filter (\connection -> connection `Map.member` connectionsMap && connection `Set.notMember` visited && visited `Set.isSubsetOf` (connectionsMap Map.! connection)) $
+                Set.toList $
+                  connectionsMap Map.! current
+       in if null possibilities
+            then [current]
+            else current : maximumBy (\a b -> length a `compare` length b) possibilities
+
+mapWithState :: (b -> a -> (c, b)) -> b -> [a] -> [c]
+mapWithState _ _ [] = []
+mapWithState function state (current : remaining) =
+  let (mappedCurrent, newState) = function state current
+   in mappedCurrent : mapWithState function newState remaining
 
 main = do
   contents <- getContents
   let connections = map ((\[a, b] -> (a, b)) . split (== '-')) $ lines contents
-      connectionsMap = listToMapOfLists (connections ++ map (\(a, b) -> (b, a)) connections)
-      ts = filter ((== 't') . head) $ map fst $ Map.toList connectionsMap
-      loops = Set.unions $ map (findLoops connectionsMap) ts
-  print $ length loops
+      connectionsMap = listToMapOfSets (connections ++ map (\(a, b) -> (b, a)) connections)
+      computers = Set.toList $ Set.fromList $ map fst connections
+      loops = Set.unions $ map (findLoops connectionsMap) computers
+      groups =
+        Set.toList
+          $ Set.fromList
+          $ mapWithState
+            ( \state loop ->
+                (sort $ tail loop ++ findBiggestConnection state (head loop) (Set.fromList loop), foldr Map.delete state loop)
+            )
+            connectionsMap
+          $ Set.toList loops
+      largestGroup = maximumBy (\a b -> length a `compare` length b) groups
+  putStrLn $ foldr1 (\val acc -> val ++ "," ++ acc) largestGroup
